2c95af87a753adaca959c37707c0116939f8685d
Flávio Monteiro <flaviomonteiro2013@gmail.com>
2016-11-17 22:40:21 -0200

    Create JsCheckup class
    This is a simple class for testing wheter the browser supports the
    required features for img2musicXML.

--- img2musicxml.js
+++ img2musicxml.js
@@ -7,3 +7,24 @@
 
 // Create img2musicXML (abbreviated as i2mx) namespace
 var i2mx = im2x || { };
+
+// Create JsCheckup class (@TODO: Move to inside namespace)
+var JsCheckup = function() {
+	this.divClassName = "i2mx-checkup";
+	
+	this.activate = function() {
+		var divList = document.getElementsByClassName(this.divClassName);
+		
+		// @TODO: Implement multiple divs support
+		divList[0].innerHTML = "No checlist for now, but img2musicXML loaded successfully!"; // @TODO: Multi-line
+	}
+	
+}
+
+var jsCheckup = new JsCheckup();
+
+// @TODO: Check window.onload vs document.onload 
+// @TODO: Use event listener instead of onload
+document.onload = function() {
+	jsCheckup.activate();
+}
