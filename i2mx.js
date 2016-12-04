3c5ddde94ed0e09244a7fa7dc5ffbc3b4d81feed
FlÃ?Â¡vio Monteiro <flaviomonteiro2013@gmail.com>
2016-12-04 04:56:26 -0200

    Improve jsCheckup  * Make it more verbose  * Don't load any components if it fails.

--- i2mx.js
+++ i2mx.js
@@ -22,22 +22,33 @@ var JsCheckup = function() {
 	
 	this.checkEverything = function() {
 		// @TODO: Use HTML generator
-		// @TODO: Wait for this to be ready to load other components.
+		
+		// Check if i2mx can display the errors checking to the user
+		if(!(this.checkForId(this.divId))) {
+			document.documentElement.innerHTML = "i2mx fatal error."
+			return false;
+		}
 		
 		// Initial values
 		var div = document.getElementById(this.divId);
 		var EOL = "<br>"
 		var checkupText = "<pre>";
+		var errorsDetected=0;
 		
-		// Do the testing
+		// Create the checkupText
 		checkupText += "Starting tests... " + EOL;
 		
 		if(window.File && window.FileReader && window.FileList && window.Blob) {
 			checkupText += "File and Blob APIs -> OK " + EOL;
 		}
 		
-		var idList = ["i2mx-image-manager", "i2mx-img_man-file-input", 
-			"i2mx-img_man-add-file-btn", "i2mx-img_man-file-list"];
+		var idList = [
+			"i2mx-image-manager", 
+			"i2mx-img_man-file-input", 
+			"i2mx-img_man-add-file-btn",
+			"i2mx-img_man-file-list",
+			"i2mx-ver_info-last-modification"
+		];
 			
 		for(var i=0; i<idList.length; i++) {
 			var id = idList[i];
@@ -47,22 +58,50 @@ var JsCheckup = function() {
 			if(this.checkForId(id)) {
 				checkupText += "OK";
 			} else {
-				checkupText += "- ERROR! -";
+				errorsDetected++;
+				checkupText += "ERROR!";
 			}
 			
 			checkupText += EOL;
 		}
 		
-		// Update DOM
+		checkupText += EOL + errorsDetected + " errors detected" + EOL;
+		
+		if(errorsDetected>0) {
+			checkupText += EOL + "img2musicXML loading ABORTED!" + EOL
+		} else {
+			checkupText += EOL + "img2musicXML will now load itself." + EOL
+		}
 		checkupText += "</pre>";
+		
+		// Update DOM
 		div.innerHTML = checkupText;
+		
+		// Return value
+		if(errorsDetected > 0) {
+			return false;
+		}
+		
+		return true;
 	}
 }
 
 // Variables
 var jsCheckup = new JsCheckup();
 
+// Functions
+function loadEverything() {
+	// Show last modification date to user
+	var lastModification = "2016-12-04";
+	var el = document.getElementById("i2mx-ver_info-last-modification");
+	el.innerHTML=lastModification;
+}
+
 // @TODO: Use event listener instead of onload
 window.onload = function() {
-	jsCheckup.checkEverything();
+	if(!jsCheckup.checkEverything()) {
+		return;
+	}
+	
+	loadEverything();
 }
