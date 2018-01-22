4419c0144692e9e616b7cf90bfecac81992f5964
feikname <flaviomonteiro2013@gmail.com>
2018-01-22 01:54:19 -0200

    PageManager: Allow moving pages up and down
    Although its dead simple and has a horrible UX, it works

--- js/i2mx_page_manager.js
+++ js/i2mx_page_manager.js
@@ -7,6 +7,7 @@
 
 window.i2mx.PageManager = new (function() {
     this.pages = []
+    this.pagesOrder = []
     this.activePages = 0
 
     this.event = {}
@@ -14,7 +15,8 @@ window.i2mx.PageManager = new (function() {
         var newPage = {}
         newPage.assignedImage = null
 
-        i2mx.PageManager.pages.push(newPage)
+        newPageIndex = (i2mx.PageManager.pages.push(newPage) - 1)
+        i2mx.PageManager.pagesOrder.push(newPageIndex)
         i2mx.PageManager.activePages++
 
         i2mx.PageManager.render()
@@ -27,6 +29,9 @@ window.i2mx.PageManager = new (function() {
         if(i2mx.DrawingCanvas.currentPageId == page_id) {
             i2mx.DrawingCanvas.closeCurrentPage()
         }
+
+        var pageIndexInOrdering = i2mx.PageManager.pagesOrder.indexOf(page_id)
+        i2mx.PageManager.pagesOrder.splice(pageIndexInOrdering, 1)
 
         i2mx.PageManager.pages[page_id] = null
         i2mx.PageManager.activePages--
@@ -55,6 +60,26 @@ window.i2mx.PageManager = new (function() {
         }
 
         alert("You tried to use a non-image file!")
+    }
+
+    this.event.movePageUp = function() {
+        var page_id = parseInt(this.getAttribute("data-page-id"))
+
+        var pageIndexInOrdering = i2mx.PageManager.pagesOrder.indexOf(page_id)
+
+        i2mx.PageManager.pagesOrder.move(pageIndexInOrdering, (pageIndexInOrdering-1))
+
+        i2mx.PageManager.render()
+    }
+
+    this.event.movePageDown = function() {
+        var page_id = parseInt(this.getAttribute("data-page-id"))
+
+        var pageIndexInOrdering = i2mx.PageManager.pagesOrder.indexOf(page_id)
+
+        i2mx.PageManager.pagesOrder.move(pageIndexInOrdering, (pageIndexInOrdering+1))
+
+        i2mx.PageManager.render()
     }
 
     this.event.deassignImageFromPage = function() {
@@ -112,15 +137,38 @@ window.i2mx.PageManager = new (function() {
         pageOpenBtn.value = "Open in canvas"
         pageOpenBtn.setAttribute("data-page-id", id)
 
+        // Create "Move page up" button
+        var movePageUpBtn = document.createElement("input")
+        movePageUpBtn.type = "button"
+        movePageUpBtn.classList.add("i2mx-page_mngr-move-item-up")
+        movePageUpBtn.classList.add("button-as-anchor")
+        movePageUpBtn.classList.add("blue-btn")
+        movePageUpBtn.value = "Up"
+        movePageUpBtn.setAttribute("data-page-id", id)
+
+        // Create "Move page down" button
+        var movePageDownBtn = document.createElement("input")
+        movePageDownBtn.type = "button"
+        movePageDownBtn.classList.add("i2mx-page_mngr-move-item-down")
+        movePageDownBtn.classList.add("button-as-anchor")
+        movePageDownBtn.classList.add("blue-btn")
+        movePageDownBtn.value = "Down"
+        movePageDownBtn.setAttribute("data-page-id", id)
+
         var img = this.pages[id].assignedImage
         if(img == null) {
-            var HTML = ord + " - (id="+id+") <b>Assign an image to this page to open it in the canvas!</b> (" +
-                pageDeleteBtn.outerHTML + ") (" + assignImgBtn.outerHTML + ")<br>"
+            var HTML = ord + "- <b>Assign an image to this page to open it in the canvas!</b> (" +
+                pageDeleteBtn.outerHTML + ") (" + assignImgBtn.outerHTML + ")"
         } else {
-            var HTML = ord + " - (id="+id+") <pre class=i2mx-page_mngr-file-name>" + img.name + "</pre>  (" +
+            var HTML = ord + " - <pre class=i2mx-page_mngr-file-name>" + img.name + "</pre>  (" +
                 pageDeleteBtn.outerHTML + ") (" + deassignImgBtn.outerHTML + ") (" +
-                pageOpenBtn.outerHTML + ")<br>"
+                pageOpenBtn.outerHTML + ")"
         }
+
+        if(ord > 1) { HTML += "(" + movePageUpBtn.outerHTML + ")"}
+        if((ord+1) <= this.activePages) { HTML += "(" + movePageDownBtn.outerHTML + ")" }
+
+        HTML += "<br>"
 
         return HTML
     }
@@ -136,11 +184,10 @@ window.i2mx.PageManager = new (function() {
         }
 
         var newHTML = ""
-        var ord = 0
-        for(var id=0; id<i2mx.PageManager.pages.length; id++) {
-            if(i2mx.PageManager.pages[id] !== null) {
-                newHTML += this.createPageHTML(id, ++ord)
-            }
+        for(var i=0; i<i2mx.PageManager.activePages; i++) {
+            var page_id = i2mx.PageManager.pagesOrder[i]
+
+             newHTML += this.createPageHTML(page_id, (i+1))
         }
 
         i2mx.Elements.pageList().innerHTML = newHTML
@@ -161,6 +208,17 @@ window.i2mx.PageManager = new (function() {
         for(var i=0; i<btns.length; i++) {
             btns[i].addEventListener("click", this.event.openPageInCanvas)
         }
+
+        btns = document.getElementsByClassName("i2mx-page_mngr-move-item-up")
+        for(var i=0; i<btns.length; i++) {
+            btns[i].addEventListener("click", this.event.movePageUp)
+        }
+
+        btns = document.getElementsByClassName("i2mx-page_mngr-move-item-down")
+        for(var i=0; i<btns.length; i++) {
+            btns[i].addEventListener("click", this.event.movePageDown)
+        }
+
     }
 
     this.load = function() {
